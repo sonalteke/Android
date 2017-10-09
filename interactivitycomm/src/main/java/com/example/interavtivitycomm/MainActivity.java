ecb3e13061f9986bad02daae6c8d69ce4fb8c431
sonalteke <sonal.teke11@gmail.com>
2017-10-09 22:06:08 +0530

    Capture Image Back

--- interactivitycomm/src/main/java/com/example/interavtivitycomm/MainActivity.java
+++ interactivitycomm/src/main/java/com/example/interavtivitycomm/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void onebtn(View view) {
         Class cls = OneActivity.class;
 
